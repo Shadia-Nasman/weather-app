2a99a5521a13e6f1108db56be68e098a23bc3cc6
shadia <shadia.nasman1988@gmail.com>
2019-12-04 16:49:03 +0100

    second change

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-
 var cityElement = document.getElementById("searchInput").value;
-const weather = {};
+
 const key = "b6c0dee41c3b13be7620763da1f9352b";
 document.body.style.backgroundImage = "url('./img/bgimg.jpg')";
 
 function getWeather(cityName) {
     fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            cityName +"&units=metric"+
+            "https://api.openweathermap.org/data/2.5/forecast?q=" +
+            cityName + "&units=metric" +
             "&appid=" +
             key
         )
@@ -15,21 +14,34 @@ function getWeather(cityName) {
             return resp.json();
         }) // Convert data to json
         .then(function(data) {
-            console.log(data);
-            var celcius =parseInt(data.main.temp);
-         
-            var description = data.weather[0].description;
+            //console.log(data);
+            document.getElementById("cityName").innerHTML = data.city.name;
+            for (var i = 0; i <= 40; i++) {
 
-            document.getElementById("weather-desc").innerHTML = description;
-            document.getElementById("weather-temp").innerHTML = celcius + "&deg;";
-            document.getElementById("weather-icon").src = "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
+                var hour = data.list[i].td_txt;
+                var onHour = hour.slice(11, 13);
+                if (onHour = 15) {
+
+                    var newDiv = document.createElement("div");
+
+                    var celcius = parseInt(data.list[i].main.temp);
+                    var day = hour.getday();
+
+                    var description = data.list[i].weather[0].description;
+
+                    newDiv.innerHTML = day + "het is " + description + " , temp: " + celcius + "&deg";
+                    document.body.appendChild(newDiv);
+                    var imgDiv = document.createElement("img");
+                    imgDiv.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png";
+                    newDiv.appendChild(imgDiv);
+                }
+            }
         });
-  
+
 }
 
 document.getElementById("searchBtn").addEventListener("click", function() {
     var cityN = document.getElementById("searchInput").value;
     getWeather(cityN);
-  
-});
 
+});
